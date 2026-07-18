816b617e792c6b37f92b3358397c87bd65aecef6
lidiaqm98 <lidial_qm@hotmail.fr>
2026-07-18 10:23:59 -0500

    modificado

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,25 @@ import { createRoot } from 'react-dom/client'
 import { createHashRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import CartContent from './components/CartContent/CartContent.jsx'
-import {ContextProvider} from './Context/Context.jsx'
+import { ContextProvider } from './Context/Context.jsx'
 
 const router = createHashRouter([
   {
     path: '/',
-    element:<App />
+    element: <App />
   },
   {
     path: '/cart',
     element: <CartContent />
   }
-])
+], {
+  basename: '/tecnologieShop' // AÑADE ESTA LÍNEA EXACTA AQUÍ
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ContextProvider>
       <RouterProvider router={router} />
     </ContextProvider>
-    
   </StrictMode>,
 )
