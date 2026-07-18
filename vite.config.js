bf237c696adeca0fb79d6a2f12eedef763df5a08
lidiaqm98 <lidial_qm@hotmail.fr>
2026-07-18 10:43:13 -0500

    feat: manual deployment using docs folder

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tecnologieShop/'
+  base: '/tecnologieShop/', // Asegúrate de que el nombre coincida exactamente con tu repo
+  build: {
+    outDir: 'docs' // Esto cambiará la carpeta de salida 'dist' por 'docs'
+  }
 })
